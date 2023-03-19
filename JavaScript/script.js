2f2f7912121a3dfd619d5d553a642f74a9c56626
kumarharsh13 <bigbangkumarharsh13@gmail.com>
2023-03-19 19:02:36 +0530

    Adding Favicon Icon and Sticky Nav

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -37,3 +37,24 @@ allLinks.forEach(function(link) {
             headerEl.classList.toggle("nav-open");
     });
 });
+
+// Sticky Navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(function (entries){
+    const ent = entries[0];
+    if(ent.isIntersecting === false) {
+        document.body.classList.add("sticky");
+    }
+
+    if(ent.isIntersecting === true) {
+        document.body.classList.add("sticky");
+    }
+}, 
+
+{
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+});
+obs.observe(sectionHeroEl)
